a7351723a8be2e6240a7931957bbc88c48295cc4
Santi <santipalenque@gmail.com>
2022-07-13 10:47:36 -0300

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,15 @@
+import * as React from "react"
+
+// markup
+const IndexPage = () => {
+  return (
+    <main>
+      <title>My Personal Website</title>
+      <h1>
+        Front-End Developer - Engineering Lead
+      </h1>
+    </main>
+  )
+}
+
+export default IndexPage
